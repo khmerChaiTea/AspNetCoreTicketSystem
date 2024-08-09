5462d8a2444bb8fd584efada3505bcd474480b96
pchai <prempreychhay@gmail.com>
2024-08-08 20:57:44 -0700

    HelpdeskRole and Helpdesk Users page is active. History log moved to Helpdesk page.

---
+++ AspNetCoreTicketSystem/Controllers/HelpdeskUsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTicketSystem.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTicketSystem; // Ensure this namespace is included for Constants
+
+public class HelpdeskUsersController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public HelpdeskUsersController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var usersInRole = await _userManager.GetUsersInRoleAsync(Constants.HelpdeskRole);
+
+        var model = new HelpdeskUsersViewModel
+        {
+            HelpdeskUsers = usersInRole
+        };
+
+        return View(model);
+    }
+}
---
+++ AspNetCoreTicketSystem/Models/HelpdeskUsersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Data;
+
+namespace AspNetCoreTicketSystem.Models
+{
+    public class HelpdeskUsersViewModel
+    {
+        public HelpdeskUsersViewModel()
+        {
+            HelpdeskUsers = Array.Empty<IdentityUser>();
+        }
+
+        public IEnumerable<IdentityUser> HelpdeskUsers { get; set; }
+    }
+}
